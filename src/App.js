8011bd2637a4cf5d8c1e0209bea31bdf9ffb53af
German Renato Cobian <germancobian@hotmail.com>
2022-10-07 20:00:32 -0700

    Setup useState hooks for color, error and list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React, { useState } from 'react';
+import SingleColor from  './SingleColor';
+import Values from 'values.js';
 import './App.css';
 
+
 function App() {
+  const [color, setColor] = useState('');
+  const [error,setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
+
   return (
-    <div className="App">
-      <h1> This is a React app</h1>
-    </div>
+    <>
+      <section className="container">
+        <h3>ColorGenerator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' />
+          <button className='btn' type='submit'>Submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List Goes Here</h4>
+      </section>
+    </>
   );
 }
 
