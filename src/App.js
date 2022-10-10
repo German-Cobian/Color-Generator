338a4fb5153581a6aa95e76d4d46450133fb63d1
German Renato Cobian <germancobian@hotmail.com>
2022-10-09 17:59:34 -0700

    Add red border warning when error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import SingleColor from  './SingleColor';
 import Values from 'values.js';
-import './App.css';
+
 
 
 function App() {
@@ -25,7 +25,13 @@ function App() {
       <section className="container">
         <h3>ColorGenerator</h3>
         <form onSubmit={handleSubmit}>
-          <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' />
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025' 
+            className={`${error ? 'error' : null}`}
+          />
           <button className='btn' type='submit'>Submit</button>
         </form>
       </section>
