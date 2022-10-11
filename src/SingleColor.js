ef890db913bbadc0d7a3861c4d1efb6592a69860
German Renato Cobian <germancobian@hotmail.com>
2022-10-10 19:13:52 -0700

    Iterate over color and obtain the rgb numbers

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils'
 
-const SingleColor = () => {
+const SingleColor = ({ rgb,weight,index }) => {
+  const [alert,setAlert] = useState(false);
+  const bcg = rgb.join(',');
+  console.log(bcg);
   return <h4>single color</h4>
-}
+};
 
 export default SingleColor
