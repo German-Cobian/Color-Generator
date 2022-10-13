4af8366b953b647c8996c74de7cb1764e767104a
German Renato Cobian <germancobian@hotmail.com>
2022-10-12 22:05:54 -0700

    Display weight and hex percentage

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils'
 
-const SingleColor = ({ rgb,weight,index }) => {
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
   const [alert,setAlert] = useState(false);
   const bcg = rgb.join(',');
+  const hex = rgbToHex(...rgb)
+
   return <article className={'color'} style={{ backgroundColor:`rgb(${bcg})` }}>
-    {''}
+    <p className='percent-value'>{weight}%</p>
+    <p className='color-value'>{hex}%</p>
   </article>
 };
 
