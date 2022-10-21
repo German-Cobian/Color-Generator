0443d19af01b5ed5e7340929e25e5adae13d2d1d
German Renato Cobian <germancobian@hotmail.com>
2022-10-20 21:48:48 -0700

    Test that hexColor displays

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       </section>
       <section className="colors">
         {list.map((color, index) => {
-        return <SingleColor key={index} {...color} index={index}
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex}
         />
         })}
       </section>
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  console.log(hexColor)
   const [alert,setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb)
+  const hex = rgbToHex(...rgb);
 
   return <article className={'color'} style={{ backgroundColor:`rgb(${bcg})` }}>
     <p className='percent-value'>{weight}%</p>
