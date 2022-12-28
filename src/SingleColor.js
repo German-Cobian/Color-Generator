5513bdbe146d9019180773070ceb96a7be30654b
German Renato Cobian <germancobian@hotmail.com>
2022-12-28 15:34:58 -0800

    Add functionality of copying color code to the clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from 'react';
 import rgbToHex from './utils'
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
-  const [alert,setAlert] = useState(true);
+  const [alert,setAlert] = useState(false);
   const bcg = rgb.join(',');
   const hex = rgbToHex(...rgb);
   const hexValue = `#${hexColor}` //add # to the display of color codes
 
-  return <article className={`color ${index > 10 && 'color-light'}` } style={{ backgroundColor:`rgb(${bcg})` }}>
+
+  return <article
+      className={`color ${index > 10 && 'color-light'}` }
+      style={{ backgroundColor:`rgb(${bcg})` }}
+      onClick= {() => { setAlert(true); navigator.clipboard.writeText(hexValue)}}
+    >
     <p className='percent-value'>{weight}%</p>
     <p className='color-value'>{hexValue}%</p>
     {alert && <p className= 'alert'> copied to clipboard</p>}
